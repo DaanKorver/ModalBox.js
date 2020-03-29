b250f42eeef9fbc8b6e22a0e0a406d72e75fa4c0
Daan <daank2000@hotmail.nl>
2020-03-29 14:11:37 +0200

    Changed default values. added width/height style

--- modalbox.js
+++ modalbox.js
@@ -9,7 +9,7 @@ class ModalBox {
 
         //Checking for options.width
         if(this.options.width === undefined) {
-            this.width = '400px'
+            this.width = '40%'
         } else if(typeof this.options.width === 'number'){
             this.width = this.options.width + 'px'
         } else if (typeof this.options.width === 'string') {
@@ -26,12 +26,97 @@ class ModalBox {
 
         //Checking for options.height
         if(this.options.height === undefined) {
-            this.height = '250px'
+            this.height = 'auto'
         } else if(typeof this.options.height === 'number'){
             this.height = this.options.height + 'px'
         } else if (typeof this.options.height === 'string') {
-            if(this.options.height.slice(-2) === 'px' || this.options.height.slice(-1) === '%' || this.options.height.slice(-2) === 'vw') {
+            if(this.options.height.slice(-2) === 'px' || this.options.height.slice(-1) === '%' || this.options.height.slice(-2) === 'vw' || this.options.height.slice(-2) === 'vh') {
                 this.height = this.options.height
+            } else {
+                throw new Error('Height is not the correct unit. Choose between px, % or vw')
+                return
+            }
+        } else {
+            throw new Error('Height must be a Number or String')
+            return
+        }
+
+        //Checking for options.maxWidth
+        if(this.options.maxWidth === undefined) {
+            this.maxWidth = '500px'
+        } else if(typeof this.options.maxWidth === 'number'){
+            this.maxWidth = this.options.maxWidth + 'px'
+        } else if (typeof this.options.maxWidth === 'string') {
+            if(this.options.maxWidth.slice(-2) === 'px' || this.options.maxWidth.slice(-1) === '%' || this.options.maxWidth.slice(-2) === 'vw') {
+                this.maxWidth = this.options.maxWidth
+            } else {
+                throw new Error('maxWidth is not the correct unit. Choose between px, % or vw')
+                return
+            }
+        } else {
+            throw new Error('maxWidth must be a Number or String')
+            return
+        }
+
+        //Checking for options.maxHeight
+        if(this.options.maxHeight === undefined) {
+            this.maxHeight = '100vh'
+        } else if(typeof this.options.maxHeight === 'number'){
+            this.maxHeight = this.options.maxHeight + 'px'
+        } else if (typeof this.options.maxHeight === 'string') {
+            if(this.options.maxHeight.slice(-2) === 'px' || this.options.maxHeight.slice(-1) === '%' || this.options.maxHeight.slice(-2) === 'vw') {
+                this.maxHeight = this.options.maxHeight
+            } else {
+                throw new Error('maxHeight is not the correct unit. Choose between px, % or vw')
+                return
+            }
+        } else {
+            throw new Error('maxHeight must be a Number or String')
+            return
+        }
+
+        //Checking for options.minWidth
+        if(this.options.minWidth === undefined) {
+            this.minWidth = '350px'
+        } else if(typeof this.options.minWidth === 'number'){
+            this.minWidth = this.options.minWidth + 'px'
+        } else if (typeof this.options.minWidth === 'string') {
+            if(this.options.minWidth.slice(-2) === 'px' || this.options.minWidth.slice(-1) === '%' || this.options.minWidth.slice(-2) === 'vw') {
+                this.minWidth = this.options.minWidth
+            } else {
+                throw new Error('minWidth is not the correct unit. Choose between px, % or vw')
+                return
+            }
+        } else {
+            throw new Error('minWidth must be a Number or String')
+            return
+        }
+
+        //Checking for options.minHeight
+        if(this.options.minHeight === undefined) {
+            this.minHeight = 'auto'
+        } else if(typeof this.options.minHeight === 'number'){
+            this.minHeight = this.options.minHeight + 'px'
+        } else if (typeof this.options.minHeight === 'string') {
+            if(this.options.minHeight.slice(-2) === 'px' || this.options.minHeight.slice(-1) === '%' || this.options.minHeight.slice(-2) === 'vw') {
+                this.minHeight = this.options.minHeight
+            } else {
+                throw new Error('minHeight is not the correct unit. Choose between px, % or vw')
+                return
+            }
+        } else {
+            throw new Error('minHeight must be a Number or String')
+            return
+        }
+
+        //Checking for options.padding
+        if(this.options.padding === undefined) {
+            this.padding = '30px'
+        } else if(typeof this.options.padding === 'number'){
+            this.padding = this.options.padding + 'px'
+        } else if (typeof this.options.padding === 'string') {
+            if(this.options.padding.slice(-2) === 'px' || this.options.padding.slice(-1) === '%' || this.options.padding.slice(-2) === 'vw') {
+                this.padding = this.options.padding
             } else {
                 throw new Error('Height is not the correct unit. Choose between px, % or vw')
                 return
@@ -109,11 +194,17 @@ class ModalBox {
 
         this.modal.style.position = 'absolute'
         this.modal.style.display = 'none'
-        this.modal.style.top = '50%'
-        this.modal.style.left = '50%'
         this.modal.style.width = this.width
         this.modal.style.height = this.height
+        this.modal.style.maxWidth = this.maxWidth
+        this.modal.style.maxHeight = this.maxHeight
+        this.modal.style.minWidth = this.minWidth
+        this.modal.style.minHeight = this.minHeight
+        this.modal.style.padding = this.padding
+        this.modal.style.top = '50%'
+        this.modal.style.left = '50%'
         this.modal.style.backgroundColor = '#fff'
+        this.modal.style.borderRadius = '4px'
         
         this.modalBg.appendChild(this.close)
         this.modalBg.appendChild(this.modal)
